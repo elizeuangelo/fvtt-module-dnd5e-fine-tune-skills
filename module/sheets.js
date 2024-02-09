017c69030dbd8095c735b667111f9f0500f1b689
elizeuangelo <elizeu90@gmail.com>
2024-02-09 10:06:14 -0300

    tidysheet support

--- module/sheets.js
+++ module/sheets.js
@@ -1,2 +1,3 @@
 import './sheets/legacy.js';
 import './sheets/character.js';
+import './sheets/tidy5e.js';
---
+++ module/sheets/tidy5e.js
@@ -0,0 +1,4 @@
+import { register } from '../actor.js';
+import { apply } from './legacy.js';
+
+register('Tidy5eSheet', apply);
